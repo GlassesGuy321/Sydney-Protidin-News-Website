f7d7536d851387284107dd4e2466e94443c4e212
Ariq Abdullah <z5417682@ad.unsw.edu.au>
2025-03-22 15:54:14 +1100

    New posts can now be created and automatically update home page

--- api/index.js
+++ api/index.js
@@ -2,10 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const User = require('./models/User');
+const Post = require('./models/Post')
 const bcrypt = require('bcryptjs');
 const app = express();
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
+const multer = require('multer');
+const uploadMiddleware = multer({ dest: 'uploads/' });
+const fs = require('fs');
 
 const salt = bcrypt.genSaltSync(10);
 const secret = 'fsaddf5243423234fd'
@@ -13,6 +17,7 @@ const secret = 'fsaddf5243423234fd'
 app.use(cors({credentials:true,origin:'http://localhost:3000'}));
 app.use(express.json());
 app.use(cookieParser());
+app.use('/uploads', express.static(__dirname + '/uploads'));
 
 mongoose.connect('mongodb+srv://blog:ZIaHR7FP9stooSho@cluster0.uqfaw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 
@@ -64,6 +69,46 @@ app.get('/profile', (req, res) => {
 
 app.post('/logout', (req, res) => {
     res.cookie('token', '').json('ok');
+})
+
+app.post('/post', uploadMiddleware.single('file'), async (req, res) => {
+    const {title, summary, imageLink, content} = req.body;
+
+    let newPath = 'undefined';
+    if (req.file) {
+        const {originalname,path} = req.file;
+        const parts = originalname.split('.');
+        const ext = parts[parts.length - 1];
+        newPath = path+'.'+ext;
+        fs.renameSync(path, newPath);
+    }
+
+    const {token} = req.cookies;
+    jwt.verify(token, secret, {}, async (err, info) => {
+        if (err) {
+            return res.status(403).json({ error: 'Invalid or expired token' });
+        }
+        const postDoc = await Post.create({
+            title,
+            summary,
+            imageLink,
+            content,
+            cover: newPath,
+            author: info.id,
+        })
+        
+        res.json(postDoc);
+    })
+    
+})
+
+app.get('/post', async (req, res) => {
+    res.json(
+        await Post.find()
+            .populate('author', ['username'])
+            .sort({createdAt: -1})
+            .limit(20)
+    );
 })
 
 app.listen(4000);
--- client/src/Post.js
+++ client/src/Post.js
@@ -1,16 +1,18 @@
-export default function Post() {
+import {format} from 'date-fns';
+
+export default function Post({title, summary, imageLink, cover, content, createdAt, author}) {
     return (
         <section className="post">
         <div className="preview">
-          <img src="https://scontent.fsyd13-1.fna.fbcdn.net/v/t39.30808-6/480966586_1278901000905707_2024376973306037719_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=833d8c&_nc_ohc=dQ9nKR93Rq0Q7kNvgHj8lsU&_nc_oc=AdkYUSUeVP635vVEndarHC_SOhnx-UHdrkqsPBsjpiSLi3eLXAFfo7eMCtI6dzNniq4&_nc_zt=23&_nc_ht=scontent.fsyd13-1.fna&_nc_gid=rKWWIIKA1OBBQH6o8BI1nA&oh=00_AYGVMNJ4USg1h5_TDFu2_8dZU0jqGVgPAAxM9N7zw2-i_g&oe=67E2CA04" alt="Banner" />
+          <img src={imageLink} alt="Banner" />
         </div>
         <div className="texts">
-          <h2> লাকেম্বা ব্রাঞ্চ লিবারেল পার্টি অব অস্ট্রেলিয়া, নিউ সাউথ ওয়েলস ডিভিশনের ইফতার ও ডিনার </h2>
+          <h2> {title} </h2>
           <p className="info">
-            <a href="" className="author">Naiem Abdullah</a>
-            <time>2025-03-21 17:05</time>
+            <a href="" className="author">{author.username}</a>
+            <time>{format(new Date(createdAt), 'MMM d, yyyy HH:mm')}</time>
           </p>
-          <p className="summary">২০ মার্চ (বৃহস্পতিবার) পবিত্র রমজান মাসের সৌন্দর্য ও ইসলামী চেতনার প্রতিফলন ঘটাতে লাকেম্বা ব্রাঞ্চ, লিবারেল পার্টি অব অস্ট্রেলিয়া (নিউ সাউথ ওয়েলস ডিভিশন) ইফতার ও ডিনারের আয়োজন করে।ব্যাংকস্টাউনের ‘লা ক্যাসটেল ভেন্যু’-তে এই আয়োজনে সমাজের বিভিন্ন স্তরের গণ্যমান্য ব্যক্তি ও লিবারেল পার্টির উচ্চ পর্যায়ের নেতৃবৃন্দ উপস্থিত ছিলেন।</p>
+          <p className="summary">{summary}</p>
         </div>
       </section>
     );
