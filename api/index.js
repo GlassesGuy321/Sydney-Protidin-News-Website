170238c74b6f5f8061db5da2f0150c83b525a16a
Ariq Abdullah <z5417682@ad.unsw.edu.au>
2025-03-23 10:57:04 +1100

    Updated packages and scripts in json files

--- api/index.js
+++ api/index.js
@@ -10,16 +10,17 @@ const cookieParser = require('cookie-parser');
 const multer = require('multer');
 const uploadMiddleware = multer({ dest: 'uploads/' });
 const fs = require('fs');
+require('dotenv').config()
 
 const salt = bcrypt.genSaltSync(10);
-const secret = 'fsaddf5243423234fd'
+const secret = process.env.SECRET;
 
 app.use(cors({credentials:true,origin:'http://localhost:3000'}));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
 
-mongoose.connect('mongodb+srv://blog:ZIaHR7FP9stooSho@cluster0.uqfaw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+mongoose.connect(`${process.env.MONGOOSE_API_KEY}`);
 
 app.post('/register', async (req, res) => {
     const {username, password} = req.body;
