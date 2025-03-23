6a0079a9d611262b2552c0354b058357303b9e38
Ariq Abdullah <z5417682@ad.unsw.edu.au>
2025-03-23 11:34:46 +1100

    Updated CORS value of backend

--- api/index.js
+++ api/index.js
@@ -17,7 +17,7 @@ const port = process.env.PORT || 4000;
 const salt = bcrypt.genSaltSync(10);
 const secret = process.env.SECRET;
 
-app.use(cors({credentials:true,origin:'http://localhost:3000'}));
+app.use(cors({credentials:true,origin:'https://GlassesGuy321.github.io/Sydney-Protidin-News-Website'}));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
