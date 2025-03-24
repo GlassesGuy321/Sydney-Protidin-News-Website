495342d2fc3501299ca6ac0f5e55f0596dbd6a60
Ariq Abdullah <z5417682@ad.unsw.edu.au>
2025-03-24 16:05:53 +1100

    Added tag lists and featured post

--- client/src/pages/IndexPage.js
+++ client/src/pages/IndexPage.js
@@ -2,9 +2,11 @@ import { useEffect, useState } from "react";
 import Post from "../Post";
 import { Link } from "react-router-dom";
 import Marquee from "react-fast-marquee";
+import Card from "../Card";
 
 export default function IndexPage() {
     const [posts, setPosts] = useState([]);
+    const tags = ['Technology', 'Health', 'Finance', 'Education', 'Entertainment'];
     useEffect(() => {
         fetch(`${process.env.REACT_APP_BACKEND_URL}/post`).then(response => {
             response.json().then(posts => {
@@ -29,6 +31,42 @@ export default function IndexPage() {
                     ))}
                 </Marquee>
             </div>
+            
+            <div className="tag-headline">
+                <h2> Hot right now </h2>
+            </div>
+
+            {posts.length > 0 && posts
+                .slice(0, 1)  // This ensures only the first/latest post is shown
+                .map(post => (
+                    <Card key={post._id} {...post} isLatest={true} />
+                ))
+            }
+
+            <div className="latest-tags">
+                {tags.map(tag => (
+                    <div className="tag-container" key={tag}>
+                        <div className="tag-headline">
+                            <Link to={`/${tag}`}>
+                                <h2>{tag}</h2>    
+                            </Link>
+                        </div>
+                        {posts.length > 0 && posts
+                            .filter(post => post.tags.includes(tag)) // Filter posts by tag
+                            .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt)) // Sort by date (newest first)
+                            .slice(0, 3) // Limit to the first 3 posts
+                            .map(post => (
+                                <Card key={post._id} {...post} />
+                        ))}
+                    </div>
+                ))}
+            </div>
+
+
+            <div className="tag-headline">
+                <h2> All News </h2>
+            </div>
+
             {posts.length > 0 && posts.map(post => (
                 <Post key={post._id} {...post} />
             ))}
